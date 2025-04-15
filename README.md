7df9aaf17d717c6e2849687f8e37fd57d89edf83
Ajay <ajay58singh@gmail.com>
2025-04-15 10:05:13 -0400

    Data ingestion stage added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # chest-classifier
 This project is for classification of Chest disease using Deep learning, MLflow and DVC
+## Workflows
+
+1. Update config.yaml
+Optional -- Updating secrets.yaml (if required)
+2. Update params.yaml
+3. Update the entity
+4. Update the configuration manager in src config
+5. Update the components
+6. Update the pipeline
+7. Update the main.py
+8. Update the dvc.yaml
+ 
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from chestcancer import logger
+from chestcancer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to Chest Canccer Classification project")
 
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
